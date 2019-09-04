8084b8255d8399bee21887c19ba71af98c203458
Dirt Deodara <thecrowleaps@gmail.com>
2019-09-04 10:48:13 -0700

    wrote a subscriber function and envoked it in our server.js to subscribe to the temp network, also customized some middleware messages

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
+const subscriber = require('./lib/utils/subscriber');
 
 const app = require('./lib/app');
+
+subscriber();
 
 const PORT = process.env.PORT || 7890;
 
