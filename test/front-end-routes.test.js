2918199e066871b812768822c58c78519d947d0d
Dirt Deodara <thecrowleaps@gmail.com>
2019-09-04 15:42:43 -0700

    we created a new route to handle our front-end, have a passing test for GET all temps

---
+++ test/front-end-routes.test.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Location = require('../lib/models/Location');
+const Temp = require('../lib/models/Temp');
+
+describe('temp-network-routes tests', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('it can return all temps', async() => {
+    const location = await Location.create({
+      name: 'Mars'
+    });
+    const temp1 = JSON.parse(JSON.stringify(await Temp.create({
+      temp: 1000,
+      location: location._id
+    })));
+    const temp2 = JSON.parse(JSON.stringify(await Temp.create({
+      temp: 4,
+      location: location._id
+    })));
+
+    return request(app)
+      .get('/api/v1/temptracker/temps')
+      .then(res=> {
+        expect(res.body).toEqual([temp1, temp2]);
+      });
+  });
+});
